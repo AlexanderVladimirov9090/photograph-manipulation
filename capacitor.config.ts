980cc251db82698b4867819908e1a0e6cf0a2047
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2023-06-05 10:23:25 +0300

    Initial skeleton application

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Photograph Manipulation App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
